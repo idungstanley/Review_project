0607e02a607f7a608415208d7c178dda8eb72b49
sunday stanley Idung <sundaystanley56@gmail.com>
2021-07-03 23:47:28 +0100

    changed the display photo

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // local reviews data
-const reviews = [
+const reviews = [ 
   {
     id: 1,
-    name: "susan smith",
+    name: "stanley sunday",
     job: "web developer",
     img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+      "https://scontent.fabb1-1.fna.fbcdn.net/v/t1.6435-9/193261111_3019698501597796_2958086655427127219_n.jpg?_nc_cat=110&_nc_rgb565=1&ccb=1-3&_nc_sid=8bfeb9&_nc_eui2=AeGEwMd4_pUgLWbVIL-Sgs4Z-t_uQjzc8EH63-5CPNzwQWy3nXHlTCligN88NjR3WCftyLWIRiXeVLeYaL1xCyOs&_nc_ohc=Q4mcRmJY-OIAX8m2tQN&_nc_ht=scontent.fabb1-1.fna&oh=b25482aa9a3e1c42e75ff3b0cdea931b&oe=60E5425E",
     text:
-      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+      "I'm web developer and a statistician, i love fashion but money never show yet to pepper una, so i just dey lay low +1. make una no mind me i just dey jones.... To my brownie, i love yaaaa"
   },
   {
     id: 2,
